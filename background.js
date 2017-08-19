ed0d5821cd49cfe48d989f7d506b6d9745e17716
Kaivalya Rawal <kaivalyarawal45@gmail.com>
2017-08-20 03:03:18 +0530

    Adding Alt+G shortcut

--- background.js
+++ background.js
@@ -38,3 +38,20 @@ chrome.pageAction.onClicked.addListener(function(tab) {
     chrome.tabs.update({url: myNewUrl});
   });
 });
+
+chrome.commands.onCommand.addListener(function(command) {
+  var tabUrl = "unknown error"
+  chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
+     var activeTab = arrayOfTabs[0];
+     tabUrl = activeTab.url;
+  });
+  chrome.tabs.getCurrent(function (tab) {
+    var myNewUrl = "kaivalyar.github.io/htmlpreview"
+    if (tabUrl.indexOf('kaivalyar.github.io/htmlpreview/?') !== -1) { // ie if url contains 'kaivalyar.github.io/htmlpreview/?'
+        var myNewUrl = tabUrl.substring(tabUrl.indexOf('?')+1)
+    } else {
+        var myNewUrl = "http://kaivalyar.github.io/htmlpreview/?" + tabUrl;
+    }
+    chrome.tabs.update({url: myNewUrl});
+  });
+});
